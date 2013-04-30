3aef091bc46e01ca8ad1980946ef6a0f309baaf0
Nils Landt <nl@fugue>
2013-04-30 12:28:53 +0200

    up version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@promisedlandt.de"
 license          "MIT"
 description      "Installs Murmur, the official Mumble server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 %w(apt runit).each { |dep| depends dep }
 
