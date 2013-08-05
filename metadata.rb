65de62be7727a0f79d9c3146b4863b5601a60464
Morgan Blackthorne <stormerider@gmail.com>
2013-08-04 20:51:24 -0700

    Add patch to allow for config values without comment lines

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@promisedlandt.de"
 license          "MIT"
 description      "Installs Murmur, the official Mumble server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 %w(apt runit).each { |dep| depends dep }
 
